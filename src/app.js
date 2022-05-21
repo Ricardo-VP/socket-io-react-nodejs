ebdaf57833435161878512589bc5405eeea77eb9
Ricardo-VP <ricardo.vacape@gmail.com>
2022-05-20 19:50:34 -0500

    Configuring socket.io 😎

--- src/app.js
+++ src/app.js
@@ -11,6 +11,30 @@ app.use(routes);
 
 const server = http.createServer(app);
 
+/**
+ * Socket IO - Configuration
+ */
+
 const io = socketIo(server);
 
-const getApiAndEmit = "TODO";
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnected", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
